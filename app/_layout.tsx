c30f60aca91f692d133b7c2307df4d2dc81aa490
Emilio H. Correa <ehcorrea60@gmail.com>
2025-02-04 09:19:06 -0300

    feat: fontes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,34 @@
-import { Stack } from "expo-router";
+import { ThemeProvider } from 'styled-components/native';
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+import { theme } from '@/constants';
+import { useEffect } from 'react';
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'SofiaPro-Light': require('@/assets/fonts/SofiaPro-Light.ttf'),
+    'SofiaPro-Regular': require('@/assets/fonts/SofiaPro-Regular.ttf'),
+    'SofiaPro-Medium': require('@/assets/fonts/SofiaPro-Medium.ttf'),
+    'SofiaPro-SemiBold': require('@/assets/fonts/SofiaPro-SemiBold.ttf'),
+    'SofiaPro-Bold': require('@/assets/fonts/SofiaPro-Bold.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider theme={theme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,6 +1,73 @@
 import { rfvalue, rhvalue, rwvalue } from '@/utils/responsive/responsive';
 
 export const theme = {
+  colors: {
+    default: {
+      primary: {
+        main: '#8A19D6',
+        80: '#9831DA',
+        50: '#B57CE7',
+        20: '#DBC6F4',
+      },
+      secondary: {
+        main: '#82D616',
+        80: '#8FDB2D',
+        50: '#B0E56A',
+        20: '#D8F2BC',
+      },
+      tertiary: {
+        main: '#03314B',
+        80: '#1C455D',
+        50: '#5E7E8F',
+        20: '#B8C7D1',
+      },
+      neutras: {
+        white: '#FEFDFBff',
+        black: '#000000ff',
+      },
+    },
+    black: {
+      main: '#1A1D26',
+      80: '#2A2F3D',
+      50: '#4D5364',
+      20: '#6E7489',
+    },
+    white: {
+      main: '#FFFFFF',
+      80: '#FFFFFF',
+      50: '#FFFFFF',
+      20: '#FFFFFF',
+    },
+    gray: {
+      main: '#9A9FAE',
+      80: '#A8ACB9',
+      50: '#C4C7D0',
+      20: '#EBEBEB',
+    },
+    error: {
+      main: '#F30000',
+      80: '#F34C4C',
+      50: '#F38585',
+      20: '#F3BEBE',
+    },
+  },
+  font: {
+    weight: {
+      light: 'SofiaPro-Light',
+      regular: 'SofiaPro-Regular',
+      medium: 'SofiaPro-Medium',
+      semi: 'SofiaPro-SemiBold',
+      bold: 'SofiaPro-Bold',
+    },
+    size: {
+      small: rfvalue(12),
+      medium: rfvalue(15),
+      large: rfvalue(18),
+      xlarge: rfvalue(24),
+      huge: rfvalue(30),
+      xhuge: rfvalue(36),
+    },
+  },
   rfvalue,
   rwvalue,
   rhvalue,
